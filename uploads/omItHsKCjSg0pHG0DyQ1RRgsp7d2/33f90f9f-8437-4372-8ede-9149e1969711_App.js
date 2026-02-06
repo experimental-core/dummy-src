b2a7e2ebe9e5e892ff0d77701ac07f2ac0b058f8
experimental-core <153198322+experimental-core@users.noreply.github.com>
2026-02-06 13:53:12 +0530

    Upload file 33f90f9f-8437-4372-8ede-9149e1969711_App.js for user omItHsKCjSg0pHG0DyQ1RRgsp7d2

---
+++ uploads/omItHsKCjSg0pHG0DyQ1RRgsp7d2/33f90f9f-8437-4372-8ede-9149e1969711_App.js
@@ -0,0 +1,28 @@
+import Router from "./router";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <>
+      <Router />
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        toastClassName="custom-toast"
+        bodyClassName="custom-toast-body"
+        progressClassName="custom-progress"
+      />
+    </>
+  );
+}
+
+export default App;
